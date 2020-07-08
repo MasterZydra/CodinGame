7294e09341c47c25ca3d0bfa092f4060988ee356
David Hein <44842054+MasterZydra@users.noreply.github.com>
2020-07-08 12:10:23 +0200

    Use list for the board lines to reduce the complexity of character substitution

--- Practice/Puzzles/Classic Puzzle - Easy/TicTacToe/Python3.py
+++ Practice/Puzzles/Classic Puzzle - Easy/TicTacToe/Python3.py
@@ -6,7 +6,7 @@ import math
 
 board = list()
 for i in range(3):
-    line = input()
+    line = list(input())
     board.append(line)
 
 # Write an answer using print
@@ -14,18 +14,20 @@ for i in range(3):
 
 winPos = -1
 
-def checkChars(line: str) -> bool:
+def checkChars(line: list) -> bool:
     global winPos
     os = 0
     dots = 0
-    for char in line:
-        if char == 'O':
+    dotPos = 0
+    for i in range(3):
+        if line[i] == 'O':
             os += 1
-        elif char == '.':
+        elif line[i] == '.':
             dots += 1
+            dotPos = i
 
     if os == 2 and dots == 1:
-        winPos = line.find('.')
+        winPos = dotPos
         return True
 
     return False
@@ -33,38 +35,22 @@ def checkChars(line: str) -> bool:
 for i in range(3):
     # Rows
     if checkChars(board[i]):
-        board[i] = 'OOO'
+        board[i][winPos] = 'O'
         break
 
     # Cols
-    line = board[0][i] + board[1][i] + board[2][i]
-    if checkChars(line):
-        newLine = ''
-        if i > 0:
-            newLine += board[winPos][0:i]
-        newLine += 'O' + board[winPos][i+1::]
-        board[winPos] = newLine
+    if checkChars(board[0][i] + board[1][i] + board[2][i]):
+        board[winPos][i] = 'O'
         break
 
 # Dias
-linePos = board[2][0] + board[1][1] + board[0][2]
-if checkChars(linePos):
-    newLine = ''
-    if winPos > 0:
-        newLine += board[2-winPos][0:winPos]
-    newLine += 'O' + board[2-winPos][winPos + 1::]
-    board[2-winPos] = newLine
-
-lineNeg = board[0][0] + board[1][1] + board[2][2]
-if checkChars(lineNeg):
-    newLine = ''
-    if winPos > 0:
-        newLine += board[winPos][0:winPos]
-    newLine += 'O' + board[winPos][winPos + 1::]
-    board[winPos] = newLine
+if checkChars(board[2][0] + board[1][1] + board[0][2]):
+    board[2-winPos][winPos] = 'O'
+elif checkChars(board[0][0] + board[1][1] + board[2][2]):
+    board[winPos][winPos] = 'O'
 
 if winPos == -1:
     print("false")
 else:
     for line in board:
-        print(line)
+        print(''.join(line))
